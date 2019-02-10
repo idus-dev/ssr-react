80c0cb9c89ce613a2e1a434fd7559a4b6d30283a
tylee <taeyouklee@gmail.com>
2019-02-10 22:47:44 +0900

    add styled-components & handle ssr

--- src/server/middleware/serverRenderer.js
+++ src/server/middleware/serverRenderer.js
@@ -1,21 +1,25 @@
-import path from 'path';
-import fs from 'fs';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
+import { renderToString } from 'react-dom/server';
+import { ServerStyleSheet } from 'styled-components';
 import { StaticRouter } from 'react-router-dom';
+import fs from 'fs';
+import path from 'path';
+import React from 'react';
 import serialize from 'serialize-javascript';
 
 import App from '../../shared/App';
 
 export default (store, data) => (req, res) => {
     const filePath = process.env.NODE_ENV === 'production' ? path.resolve(__dirname, 'template.html') : './public/template.html';
+    const sheet = new ServerStyleSheet();
     const preloadedState = store.getState();
     const context = { data };
     const html = renderToString(
-        <Provider store={store}>
-            <StaticRouter location={req.url} context={context}><App /></StaticRouter>
-        </Provider>
+        sheet.collectStyles(
+            <Provider store={store}>
+                <StaticRouter location={req.url} context={context}><App /></StaticRouter>
+            </Provider>
+        )
     );
 
     fs.readFile(filePath, 'utf8', (err, htmlData) => {
@@ -33,6 +37,7 @@ export default (store, data) => (req, res) => {
 
         return res.send(
             htmlData
+                .replace('<style></style>', `<style>${sheet.getStyleTags()}</style>`)
                 .replace('__PRELOADED_STATE__={}', `__PRELOADED_STATE__=${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}`)
                 .replace('__INITIAL_DATA__={}', `__INITIAL_DATA__=${serialize(data)}`)
                 .replace('<div id="app"></div>', `<div id="app">${html}</div>`)
