c9fd5c0cb5ff230581182341df89cbf6d596da89
7ylee <taeyouklee@gmail.com>
2020-07-09 15:48:27 +0900

    handle default state- `ssr: true` on app mount

--- src/client/App.js
+++ src/client/App.js
@@ -6,27 +6,32 @@ import Navigation from './components/Navigation';
 import NoMatch from './pages/NoMatch';
 import ssrRoutes from '../server/ssrRoutes';
 import Main from './pages/Main';
+import useEffectSSR from './hooks/useEffectSSR';
 
-const App = () => (
-    <main>
-        <ResetCSS />
-        <Navigation />
-        {/* eslint-disable react/jsx-props-no-spreading */}
-        <Switch>
-            <Route path="/" exact component={Main} />
-            {/* routes that requires serverside rendering */}
-            {ssrRoutes.map(({ path, exact, component: C, ...rest }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    exact={exact}
-                    render={props => <C {...props} {...rest} />}
-                />
-            ))}
-            <Route render={props => <NoMatch {...props} />} />
-        </Switch>
-        {/* eslint-ensable react/jsx-props-no-spreading */}
-    </main>
-);
+const App = () => {
+    // handle default state- `ssr: true` on app mount
+    useEffectSSR();
+    return (
+        <>
+            <ResetCSS />
+            <Navigation />
+            {/* eslint-disable react/jsx-props-no-spreading */}
+            <Switch>
+                <Route path="/" exact component={Main} />
+                {/* routes that requires serverside rendering */}
+                {ssrRoutes.map(({ path, exact, component: C, ...rest }) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        exact={exact}
+                        render={props => <C {...props} {...rest} />}
+                    />
+                ))}
+                <Route render={props => <NoMatch {...props} />} />
+            </Switch>
+            {/* eslint-ensable react/jsx-props-no-spreading */}
+        </>
+    );
+};
 
 export default App;
